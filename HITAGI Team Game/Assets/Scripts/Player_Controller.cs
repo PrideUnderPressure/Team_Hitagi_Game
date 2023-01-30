00341eeaa18b4e18c794c38741d8dd9c199f4fed
PrideUnderPressure <michalgalecki.uk@gmail.com>
2023-01-30 16:20:16 +0000

    Some more tiles + character

---
+++ HITAGI Team Game/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
