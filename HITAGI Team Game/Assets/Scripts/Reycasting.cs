da8f295d82c463a8350e6c27ed0978476989e053
PrideUnderPressure <michalgalecki.uk@gmail.com>
2023-01-30 17:32:37 +0000

    Last versio

---
+++ HITAGI Team Game/Assets/Scripts/Reycasting.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reycasting : MonoBehaviour
+{
+    [SerializeField]
+    Transform raycastTop;
+
+    [SerializeField]
+    Transform raycastBottom;
+
+    [SerializeField]
+    Transform raycastLeft;
+
+    [SerializeField]
+    Transform raycastRight;
+
+    public bool topRaycastActive;
+    public bool bottomRaycastActive;
+    public bool leftRaycastActive;
+    public bool rightRaycastActive;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void TopRaycastActive()
+    {
+
+    }
+}
